90a1a9cd5c0290f24d2356c878fea91dffef343f
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-21 19:41:11 -0500

    :pencil2: [FIX] Import Typo

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react'
 import { Container } from 'semantic-ui-react'
 
-import Navbar from '@components/Navbar/Navbar'
+import Navbar from '@components/NavBar/NavBar'
 import Footer from '@components/Footer/Footer'
 
 type LayoutProps = {

99e3dcc7d5cec72c9a10bc8720d8337d4447a28e
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-23 14:43:34 -0500

    :alembic: [ADD] Function for obtain the performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import { AppProps } from 'next/app'
 import 'semantic-ui-css/semantic.min.css'
 import '../global.css'
-
 import CartProvider from '@store/Cart'
+
+/* export function reportWebVitals(metric) {
+  //* And here we can export the data of the performance and access times to external servers, like Google Analytics
+} */
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props

9da9014969f5974aa4e5050b1504c46f2a7bc148
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-23 11:04:00 -0500

    :art: [ADD] Static generation method

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,21 @@ import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-// This is for getting the loading in the server
+/* // This is for getting the loading in the server
 //* Generating Server Side Rendering
 export const getServerSideProps = async () => {
+  const response = await fetch('https://avocato-nextjs.vercel.app/api/avo') //! We need to use an absolute URL
+  const { data: productList }: TAPIAvoResponse = await response.json()
+  return {
+    props: {
+      productList,
+    },
+  }
+} */
+
+//* Static render
+export const getStaticProps = async () => {
+  //! We can only use in page components
   const response = await fetch('https://avocato-nextjs.vercel.app/api/avo') //! We need to use an absolute URL
   const { data: productList }: TAPIAvoResponse = await response.json()
   return {

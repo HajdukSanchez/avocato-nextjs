df16738ab8f660add9b176e267e02025ba738e24
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-23 13:30:09 -0500

    :art: [ADD] Static files generated

---
+++ out/_next/static/oOc1_GO9_kjwJ0yum10OH/_buildManifest.js
@@ -0,0 +1,38 @@
+self.__BUILD_MANIFEST = (function (a, b) {
+  return {
+    __rewrites: [],
+    '/': [
+      a,
+      b,
+      'static\u002Fchunks\u002Fpages\u002Findex-d4d5318cd8a72d825537.js',
+    ],
+    '/_error': [
+      'static\u002Fchunks\u002Fpages\u002F_error-584ffa6e2c9f8985f171.js',
+    ],
+    '/about': [
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fabout-5d23a2282c0668f3b7c1.js',
+    ],
+    '/cart': [
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fcart-605e30f4d8bb6d8d0cbf.js',
+    ],
+    '/product/[id]': [
+      a,
+      b,
+      'static\u002Fchunks\u002Fpages\u002Fproduct\u002F[id]-e27c38554f694530c330.js',
+    ],
+    sortedPages: [
+      '\u002F',
+      '\u002F_app',
+      '\u002F_error',
+      '\u002Fabout',
+      '\u002Fcart',
+      '\u002Fproduct\u002F[id]',
+    ],
+  }
+})(
+  'static\u002Fchunks\u002F43bd46a068bfb2583012eef9c477242c49af3e55.34dffc5950cf301b81b3.js',
+  'static\u002Fchunks\u002F86bd6d32e95993440c8793a61a113550b621c73d.af89b043f3970b5ddbc8.js'
+)
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
